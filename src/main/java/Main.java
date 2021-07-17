f9bda19931672b77654ca7e7c657ddac6c83f68b
OHHYUN <omplcie@gmail.com>
2021-07-17 23:35:59 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        solution(new int[]{3, 1, 2, 3});
+    }
+
+    public static int solution(int[] nums) {
+        int answer = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet()
+                        , p -> Integer.min(p.size(), nums.length)));
+
+        return answer;
+    }
+}
